afbe2d893e4a03491b23a56f0795260882e21c78
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-03-01 11:02:15 +0100

    :tada: init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const plugins = [ new HtmlWebpackPlugin({ template: 'src/index.html' })];
+// plugins.push(new UglifyJsPlugin());
+
+module.exports = {
+    entry: './src/main.js',
+    devtool: 'eval-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    plugins,
+    devServer : {
+        host: '0.0.0.0',
+        port: 9000
+    }
+};
