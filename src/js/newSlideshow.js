dd6142dbdea1f25be46ebea81e351225883b6e5b
Thibaud Crespin <thibaud.crespin@biig.fr>
2018-03-08 09:45:29 +0100

    Update templates and set active slide on creation

--- src/js/newSlideshow.js
+++ src/js/newSlideshow.js
@@ -3,32 +3,32 @@ import * as slide from './slideTransition';
 const MODELS = [
     {
         'name': 'model-1',
-        'template': "<div class='content'><h1 class='center' contenteditable='true'>Votre titre ici</h1></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><h1 class='center' contenteditable='true'>Votre titre ici</h1></div>",
         'choice': 1
     },
     {
         'name': 'model-2',
-        'template': "<div class='content'><h1 class='top' contenteditable='true'>Votre titre ici</h1><p class='left' contenteditable='true'>Votre texte ici</p><img class='right' src='.'><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><h1 class='top' contenteditable='true'>Votre titre ici</h1><p class='left' contenteditable='true'>Votre texte ici</p><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
         'choice': 2
     },
     {
         'name': 'model-3',
-        'template': "<div class='content'><h1 class='top' contenteditable='true'>Votre titre ici</h1><img class='left' src='.'><p class='right' contenteditable='true'>Votre texte ici</p><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><h1 class='top' contenteditable='true'>Votre titre ici</h1><p class='right' contenteditable='true'>Votre texte ici</p><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
         'choice': 3
     },
     {
         'name': 'model-4',
-        'template': "<div class='content'><h2 contenteditable='true'>Votre texte ici</h2><h1 class='top' contenteditable='true'>Votre titre ici</h1><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><h2 contenteditable='true'>Votre texte ici</h2><h1 class='top' contenteditable='true'>Votre titre ici</h1><p class='bottom' contenteditable='true'>Votre texte ici</p></div>",
         'choice': 4
     },
     {
         'name': 'model-5',
-        'template': "<div class='content'><p class='left' contenteditable='true'>Votre texte ici</p><img class='right' src='.'></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><p class='left' contenteditable='true'>Votre texte ici</p></div>",
         'choice': 5
     },
     {
         'name': 'model-6',
-        'template': "<div class='content'><img class='left' src='.'><p class='right' contenteditable='true'>Votre texte ici</p></div>",
+        'template': "<div class='bg'><img src='.'></div><div class='content'><p class='right' contenteditable='true'>Votre texte ici</p></div>",
         'choice': 6
     }
 ];
@@ -69,6 +69,19 @@ export const createChoices = (slideshow) => {
     slideshow.appendChild(choices);
 }
 
+export const addSlide = (slideshow, slide, id, model) => {
+    const current = document.querySelector('.slide.active');
+    if(current) current.classList.remove('active');
+
+    slide.removeAttribute('id');
+    slide.classList.remove('model');
+    slide.classList.add('slide', 'active');
+    slide.dataset.slideId = id;
+    slide.dataset.slideModel = model;
+
+    return slide;
+}
+
 export const insert = (slideshow, workspace) => {
     createModels(slideshow);
     createChoices(slideshow);
@@ -84,14 +97,10 @@ export const init = (slideshow, workspace) => {
         const model = document.querySelector('#model-' + modelId);
         let tmp = model.cloneNode(true);
         cpt++;
+
+        let res = addSlide(slideshow, tmp, cpt, modelId);
     
-        tmp.removeAttribute('id');
-        tmp.classList.remove('model');
-        tmp.classList.add('slide', 'active');
-        tmp.dataset.slideId = cpt;
-        tmp.dataset.slideModel = modelId;
-    
-        workspace.appendChild(tmp);
+        workspace.appendChild(res);
     }
     
     choicesEl.forEach((element) => {
